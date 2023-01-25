cb82acda9626ef2a44f77ffd51cee7e7d26c06d1
andreadldev <andrea.delisi2@gmail.com>
2023-01-25 16:43:33 +0100

    Autenticazione e route/layout admin

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
